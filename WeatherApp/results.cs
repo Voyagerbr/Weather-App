29ab77e2ff478adfd4322344a37e29f4719090a9
Nicolas <nicolasdealmeidasoares71@gmail.com>
2024-04-17 09:51:47 -0300

    Second commit

---
+++ WeatherApp/results.cs
@@ -0,0 +1,13 @@
+namespace WeatherApp;
+
+public partial class results
+{
+    public int temp {get; set;}
+    public string time {get; set;}
+    public string date {get; set;}
+    public int total {get; set;}
+	public results()
+	{
+
+	}
+}
